ff66193f29c9ce7af3f5e996bf54daea39ed830b
Pratiksha Pandharinath Hake <122038101+PratikshaHake@users.noreply.github.com>
2024-07-28 20:28:23 +0530

    Delete Mail sender automation project/Demo directory

--- Mail sender automation project/Demo/connection - Copy.py
+++
@@ -1,8 +0,0 @@
-import urllib.request as urllib2
-def is_connected():
-    print("Inside connection")
-    try:
-        urllib2.urlopen('http://www.google.com',timeout=1)
-        return True
-    except urllib2.URLError as err:
-        return False
